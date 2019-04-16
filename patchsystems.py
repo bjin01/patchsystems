489c3dd22f06f002277762af54aec9f6273cdced
bjin01 <bo.jin@suse.com>
2019-04-16 08:28:56 +0200

    adding v1

---
+++ patchsystems.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+import xmlrpclib,  argparse,  getpass,  textwrap,  sys
+from datetime import datetime
+from array import array
+
+class Password(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string):
+        if values is None:
+            values = getpass.getpass()
+
+        setattr(namespace, self.dest, values)
+
+parser = argparse.ArgumentParser()
+#parser.add_argument("-v", "--verbosity", action="count", default=0)
+parser = argparse.ArgumentParser(prog='PROG', formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
+This scripts runs service pack migration for given base channel. 
+
+Sample command:
+
+              python patchsystems.py -s bjsuma.bo2go.home -u bjin -p suse1234 -g DEV-SLES12SP3 \n \
+
+If -x is not specified the SP Migration is always a dryRun.
+Check Job status of the system if dryrun was successful before run the above command with -x specified. ''')) 
+parser.add_argument("-x", "--patch-it", action="store_true")
+parser.add_argument("-s", "--server", help="Enter your suse manager host address e.g. myserver.abd.domain",  default='localhost',  required=True)
+parser.add_argument("-u", "--username", help="Enter your suse manager loginid e.g. admin ", default='admin',  required=True)
+parser.add_argument('-p', action=Password, nargs='?', dest='password', help='Enter your password',  required=True)
+parser.add_argument("-g", "--group_name", help="Enter a valid groupname. e.g. DEV-SLES12SP3 ",  required=True)
+args = parser.parse_args()
+
+MANAGER_URL = "http://"+ args.server+"/rpc/api"
+MANAGER_LOGIN = args.username
+MANAGER_PASSWORD = args.password
+session_client = xmlrpclib.Server(MANAGER_URL, verbose=0)
+session_key = session_client.auth.login(MANAGER_LOGIN, MANAGER_PASSWORD)
+today = datetime.today()
+earliest_occurrence = xmlrpclib.DateTime(today)
+
+allgroups = session_client.systemgroup.listAllGroups(session_key)
+for a in allgroups:
+    print("Group name: %s\t with %s systems." %(a['name'],  str(a['system_count'])))
+
+if args.group_name:
+    try:
+        systemlist = session_client.systemgroup.listActiveSystemsInGroup(session_key, args.group_name)
+        print("system group %s found!" %(args.group_name))
+    except:
+        print("something went wrong. The groupname could not be validated.")
+else:
+    sys.exit(1)
+
+for s in systemlist:
+    print(s)
+    packagelist = session_client.system.listLatestUpgradablePackages(session_key, s)
+    print("total number of upgradable packages are: ",  len(packagelist))
+    #for p in packagelist:
+        #print(p)
+    #print("system name: %s" %(str(systemlist['server_id'])))
+
+finalpatches = array("i")
+
+for e in systemlist:
+    erratalist = session_client.system.getRelevantErrata(session_key, s)
+    print(erratalist)
+    for r in erratalist:
+        finalpatches.append(r['id'])
+        #print(finalpatches)
+        #try:
+        jobid = session_client.system.scheduleApplyErrata(session_key, s,  r['id'],  earliest_occurrence)
+        print(jobid)
+        #except:
+        #    print()
+
+            
