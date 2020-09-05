5aec36d751b698437b1950a22f47db4c84864bbf
bjin01 <bo.jin@suse.com>
2020-09-05 16:42:47 +0200

    add s script only for ubuntu upgrade jobs

---
+++ update_ubuntu_systemsByGroupWithRebootV2.py
@@ -0,0 +1,197 @@
+#!/usr/bin/python
+import xmlrpclib,  argparse,  getpass,  textwrap,  json, sys
+from datetime import datetime,  timedelta
+from collections import defaultdict
+#from array import *
+
+class Password(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string):
+        if values is None:
+            values = getpass.getpass()
+        setattr(namespace, self.dest, values)
+
+parser = argparse.ArgumentParser()
+reboot_parser = argparse.ArgumentParser()
+#parser.add_argument("-v", "--verbosity", action="count", default=0)
+parser = argparse.ArgumentParser(prog='PROG', formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
+This scripts schedules patch deployment jobs for given group's systems' in given hours from now on. A reboot will be scheduled as well. 
+Sample command:
+              python updatesystemsByGroupWithReboot.py -s bjsuma.bo2go.home -u bjin -p suse1234 -g testgroup -o 2  \n \
+              python updatesystemsByGroupWithReboot.py -s bjsuma.bo2go.home -u bjin -p suse1234 -g testgroup -sr 15:30 20-04-2020  \n \
+              python updatesystemsByGroupWithReboot.py -s bjsuma.bo2go.home -u bjin -p suse1234 -g testgroup -r true  \n \
+              python updatesystemsByGroupWithReboot.py -s bjsuma.bo2go.home -u bjin -p suse1234 -g testgroup -o 1 -sr 15:30 20-04-2020 -r true  \n \
+Check Job status of the system. ''')) 
+parser.add_argument("-s", "--server", help="Enter your suse manager host address e.g. myserver.abd.domain",  default='localhost',  required=True)
+parser.add_argument("-u", "--username", help="Enter your suse manager loginid e.g. admin ", default='admin',  required=True)
+parser.add_argument('-p', action=Password, nargs='?', dest='password', help='Enter your password',  required=True)
+parser.add_argument("-g", "--group_name", help="Enter a valid groupname. e.g. DEV-SLES12SP3 ",  required=True)
+parser.add_argument("-o", "--in_hours", help="in how many hours should the job be started. e.g. 2 ",  required=False)
+parser.add_argument("-sr", "--schedule_reboot", help="when it should reboot in format 15:30 20-04-1970",  required=False)
+# parser.add_argument("-r", "--reboot", help="if this optional argument is provided then a reboot jobs schedules \
+#     for one hour later then patch jobs will be scheduled as well.", default=False, required=True)
+parser.add_argument("-os", "--os_type", help="you can specify if this is e.g. -os utuntu ", required=False)
+
+reboot_parser = parser.add_mutually_exclusive_group(required=True)
+reboot_parser.add_argument("-r", '--reboot', dest='reboot', action='store_true')
+reboot_parser.add_argument("-no-r", '--no-reboot', dest='reboot', action='store_false')
+parser.set_defaults(reboot=False)
+args = parser.parse_args()
+
+
+MANAGER_URL = "http://"+ args.server+"/rpc/api"
+MANAGER_LOGIN = args.username
+MANAGER_PASSWORD = args.password
+
+#This is a new reboot request to allow reboot in given hours OR given exact time schedule.
+#But if both params provide than we exit as this can not be handled at same time.
+""" if args.schedule-reboot and args.in_hours:
+    print("You can not provide --in_hours and --schedule-reboot at the same command. It is either or.")
+    sys.exit(1) """
+
+session_client = xmlrpclib.Server(MANAGER_URL, verbose=0)
+session_key = session_client.auth.login(MANAGER_LOGIN, MANAGER_PASSWORD)
+
+if args.in_hours:
+    check_install_time = datetime.now() + timedelta(hours=int(args.in_hours))
+else:
+    check_install_time = datetime.now()
+if args.schedule_reboot:
+    check_schedule_reboot_time = datetime.strptime(args.schedule_reboot, "%H:%M %d-%m-%Y")
+else:
+    check_schedule_reboot_time = datetime.now() + timedelta(minutes=10)
+
+
+if args.reboot == True:
+    if check_install_time and check_schedule_reboot_time:
+        if check_schedule_reboot_time <= check_install_time:
+            print("Error: Your desired reboot schedule time happens earlier than your desired install time. That is not a good idea.")
+            print("Please define a proper reboot time after scheduled update time. e.g. -sr '15:30 20-04-2020'")
+            sys.exit(1)
+        elif check_schedule_reboot_time <= datetime.now():
+            print("Error: Your desired reboot schedule time happens earlier than current time. That is not a good idea.")
+            sys.exit(1)
+
+
+
+if args.in_hours:
+    nowlater = datetime.now() + timedelta(hours=int(args.in_hours))
+else:
+    nowlater = datetime.now()
+earliest_occurrence = xmlrpclib.DateTime(nowlater)
+allgroups = session_client.systemgroup.listAllGroups(session_key)
+
+joblist = []
+joblist_reboot = []
+nested_dict = lambda: defaultdict(nested_dict)
+jobsdict = nested_dict
+jobsdict = {}
+error1 = 0
+
+
+def scheduleReboot(serverid,  servername):
+    if args.in_hours and not args.schedule_reboot:
+        reboottime = datetime.now() + timedelta(hours=int(args.in_hours)+1)
+    elif args.schedule_reboot and args.in_hours :
+        reboottime = datetime.strptime(args.schedule_reboot, "%H:%M %d-%m-%Y")
+    else:
+        reboottime = datetime.now() + timedelta(minutes=10)
+
+    earliest_occurrence_reboot = xmlrpclib.DateTime(reboottime)
+    reboot_jobid = session_client.system.scheduleReboot(session_key, serverid, earliest_occurrence_reboot)
+    try: 
+        if reboot_jobid:
+            system_name = session_client.system.getName(session_key, serverid)
+            print("Reboot Job ID %s for %s %s has been created" %(str(reboot_jobid),  (str(serverid)), system_name['name']))
+            jobsdict[servername]['Reboot_jobs']  = {}
+            jobsdict[servername]['serverid'] = serverid
+            jobsdict[servername]['Reboot_jobs'][reboot_jobid] = 'pending'
+
+    except NameError:
+        print("No reboot job created.")
+        sys.exit(1)
+    
+    
+def json_write(mydict):
+        with open("joblist.json", "w") as write_file:
+            json.dump(mydict, write_file,  indent=4)
+
+def update_ubuntu(session_key, e, system_updatelist, earliest_occurrence):
+    updatelist = session_client.system.listLatestUpgradablePackages(session_key, e)
+    if not updatelist:
+        system_name = session_client.system.getName(session_key, e)
+        print("All good. No Updates needed:\t %s" %(system_name['name']))
+        sys.exit(0)
+        
+    for s in updatelist:
+        system_updatelist.append(s['to_package_id'])
+    if system_updatelist:        
+        system_actionid = session_client.system.schedulePackageInstall(session_key, e, system_updatelist, earliest_occurrence)
+        del system_updatelist
+        system_name = session_client.system.getName(session_key, e)
+        jobsdict[system_name['name']]={}
+        jobsdict[system_name['name']]['Patch_jobs']  = {}
+        jobsdict[system_name['name']]['serverid']= e
+    
+        jobsdict[system_name['name']]['Patch_jobs'][system_actionid] =  'pending'
+        print("Job ID %s for %s %s has been created" %(str(system_actionid),  (str(e)), system_name['name']))
+        if args.reboot == True:
+            scheduleReboot(e,  system_name['name'])
+    else:
+        print("No package to update.")   
+        sys.exit(0)
+
+if args.group_name:
+    grpfound = 'false'
+    for a in allgroups:
+        
+        if a['name'] == args.group_name:
+            print("Targeting Ubuntu systems in group: %s with %s systems." %(a['name'],  str(a['system_count'])))
+            grpfound = 'true'
+            try:
+                activesystemlist = session_client.systemgroup.listActiveSystemsInGroup(session_key, args.group_name)
+                print("activesystemlist is: %s" %(activesystemlist))
+            except:
+                error1 = 1
+                print("uups something went wrong. We could not find the active systems in the given group name. Maybe the group is empty.")
+            break
+    
+    if grpfound == 'false':
+        print("sorry we could not find the group you provided. Check if it exists or case-sensitive name correctness.")
+        sys.exit(1)
+    
+    
+    for e in activesystemlist:
+        system_updatelist = []
+
+        if args.os_type and 'ubuntu' in args.os_type.lower():
+            system_products = session_client.system.getInstalledProducts(session_key, e)
+            if system_products:
+                for a in system_products:
+                    if "ubuntu" in a['friendlyName'].lower():
+                        ubuntu = True
+            
+            try: ubuntu
+            except NameError:
+                print("No ubuntu system found.")
+                sys.exit(1)
+            else:
+                if ubuntu:
+                    update_ubuntu(session_key, e, system_updatelist, earliest_occurrence)
+        else: 
+            print("You did not specify -os for a os type e.g. -os ubuntu")
+            sys.exit(1)
+                
+        """ except:
+            error1 = 1
+            system_name = session_client.system.getName(session_key, e)
+            print("uups something went wrong. We could not create scheduleApplyErrata for:\t %s." %(system_name['name']))
+            print("one possible reason is the targeted systems already have pending patch and reboot jobs scheduled.") """
+
+if error1 != 1:
+    json_write(jobsdict)
+session_client.auth.logout(session_key)
+
+
+
+
+            
