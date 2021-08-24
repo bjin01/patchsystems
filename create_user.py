68cff4a74430b4690afe63a34ccb47db3efc0b3d
Bo Jin <bjin01@users.noreply.github.com>
2021-08-24 17:02:08 +0200

    Update create_user.py

--- create_user.py
+++ create_user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import yaml
-import os 
+import os
 import argparse
 import getpass
 import textwrap
@@ -10,11 +10,12 @@ from datetime import datetime,  timedelta
 from xmlrpc.client import ServerProxy, Error
 
 def get_login(path):
-    
+
     if path == "":
         path = os.path.join(os.environ["HOME"], "suma_config.yaml")
     with open(path) as f:
         login = yaml.load_all(f, Loader=yaml.FullLoader)
+        print("see yaml: %s" % login)
         for a in login:
             login = a
 
@@ -26,9 +27,9 @@ def login_suma(login):
     MANAGER_PASSWORD = login['suma_password']
     SUMA = "http://" + login['suma_user'] + ":" + login['suma_password'] + "@" + login['suma_host'] + "/rpc/api"
     with ServerProxy(SUMA) as session_client:
-
-    #session_client = xmlrpclib.Server(MANAGER_URL, verbose=0)
+        print("%s %s %s" % (login['suma_host'], login['suma_user'], login['suma_password']))
         session_key = session_client.auth.login(MANAGER_LOGIN, MANAGER_PASSWORD)
+        print("sessionkey: %s" % session_key)
     return session_client, session_key
 
 def suma_logout(session, key):
@@ -36,16 +37,16 @@ def suma_logout(session, key):
     return
 
 def get_suma_users(session, key):
-    suma_users = session.users.listUsers(key)
+    suma_users = session.user.listUsers(key)
     for i in suma_users:
         print("login: %s " % i['login'])
     return suma_users
 
 def get_ad_users(ad_group):
-    cmd = 'getent group'
-    temp = subprocess.Popen([cmd, ad_group], stdout = subprocess.PIPE) 
+    cmd = '/usr/bin/getent'
+    temp = subprocess.Popen([cmd, 'group', ad_group], stdout = subprocess.PIPE)
         # get the output as a string
-        output = str(temp.communicate())
+    output = str(temp.communicate())
     print("users from AD: %s" % output)
 
     return output
@@ -53,15 +54,15 @@ def get_ad_users(ad_group):
 parser = argparse.ArgumentParser()
 #parser.add_argument("-v", "--verbosity", action="count", default=0)
 parser = argparse.ArgumentParser(prog='PROG', formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
-This script creates users from AD and delete users which does not exist anymore. 
+This script creates users from AD and delete users which does not exist anymore.
 Sample command:
               python3 create_user.py -f suma_config.yaml \n \
-''')) 
-parser.add_argument("-c", "--config", help="Enter config file in yaml format that holds login.",  default='./suma_config.yaml',  required=True))
-parser.add_argument("-g", "--group", help="Enter AD group name.",  default='testgroup',  required=True))
+'''))
+parser.add_argument("-c", "--config", help="Enter config file in yaml format that holds login.",  default='./suma_config.yaml',  required=True)
+parser.add_argument("-g", "--group", help="Enter AD group name.",  default='testgroup',  required=True)
 args = parser.parse_args()
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     suma_login = get_login(args.config)
     session, key = login_suma(suma_login)
     suma_users = get_suma_users(session, key)
