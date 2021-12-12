2ef479ebb5a6a4f9de8fdf6cb001c68ecf93bebd
Bo Jin <bo.jin@suse.com>
2021-12-12 14:16:16 +0100

    minior updates

--- update_klp.py
+++ update_klp.py
@@ -1,29 +1,68 @@
 #!/usr/bin/python
 
-import argparse,  getpass,  textwrap, time
+import subprocess
+import argparse
+#import getpass
+import textwrap
+#import time
 import datetime
 import yaml
 import os
-from xmlrpc.client import ServerProxy, Error, DateTime
+from xmlrpc.client import ServerProxy, DateTime
+import logging
+import shutil
+from os import access, R_OK
+from os.path import isfile
 
-class Password(argparse.Action):
+
+logfilename = "/var/log/klp_deploy.log"
+mylogs = logging.getLogger(__name__)
+mylogs.setLevel(logging.DEBUG)
+#file handler adding here, log file should be overwritten every time as this will be sent via email
+file = logging.FileHandler(logfilename, mode='w')
+file.setLevel(logging.DEBUG)
+
+fileformat = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s",datefmt="%H:%M:%S")
+file.setFormatter(fileformat)
+
+#handler for sending messages to console stdout
+stream = logging.StreamHandler()
+streamformat = logging.Formatter("%(asctime)s:%(filename)s:%(levelname)s:%(message)s",datefmt="%H:%M:%S")
+stream.setLevel(logging.DEBUG)
+stream.setFormatter(streamformat)
+
+mylogs.addHandler(file)
+mylogs.addHandler(stream)
+
+""" class Password(argparse.Action):
     def __call__(self, parser, namespace, values, option_string):
         if values is None:
             values = getpass.getpass()
 
-        setattr(namespace, self.dest, values)
+        setattr(namespace, self.dest, values) """
 
 parser = argparse.ArgumentParser()
 parser = argparse.ArgumentParser(prog='PROG', formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
-This scripts helps to deploy kernel live patching patches  
+This is SUSE Live Patching patch deployment tool to update existing systems with SUSE Live Patching already installed.
+
+You need a suma_config.yaml file with login and email notification address.
+If email notification will be used then you need to have mutt email client installed. 
+
+Sample suma_config.yaml:
+suma_host: mysumaserver.mydomain.local
+suma_user: <USERNAME>
+suma_password: <PASSWORD>
+notify_email: <EMAIL_ADDRESS>
+
 Sample command:
               python3 update_klp.py --config /root/suma_config.yaml --group api_group_test
-The script helps to deploy kernel live patching patches to a group of systems. '''))
+              python3 update_klp.py --config /root/suma_config.yaml --group api_group_test
+The script helps to deploy kernel live patching patches to a group of systems and send email notifications optionally.'''))
 
-parser.add_argument("--config", help="Enter the config file name that contains login and channel information e.g. /root/suma_config.yaml",  required=False)
-parser.add_argument("--group", help="Enter the group name for which systems of a group you want to change channels. e.g. testsystems",  required=False)
+parser.add_argument("--config", help="enter the config file name that contains login information e.g. /root/suma_config.yaml",  required=False)
+parser.add_argument("--group", help="Enter the group name for which systems of a group you want to install live kernel patches.",  required=False)
+parser.add_argument("--email", help="use this option if you want email notifcation, the log file will be sent to it. The email address is provided in the suma_config.yaml",  action="store_true")
 args = parser.parse_args()
-
 
 def get_login(path):
     
@@ -37,13 +76,10 @@ def get_login(path):
     return login
 
 def login_suma(login):
-    MANAGER_URL = "https://"+ login['suma_host'] +"/rpc/api"
     MANAGER_LOGIN = login['suma_user']
     MANAGER_PASSWORD = login['suma_password']
     SUMA = "http://" + login['suma_user'] + ":" + login['suma_password'] + "@" + login['suma_host'] + "/rpc/api"
     with ServerProxy(SUMA) as session_client:
-
-    #session_client = xmlrpclib.Server(MANAGER_URL, verbose=0)
         session_key = session_client.auth.login(MANAGER_LOGIN, MANAGER_PASSWORD)
     return session_client, session_key
 
@@ -51,8 +87,27 @@ def suma_logout(session, key):
     session.auth.logout(key)
     return
 
+def result2email():
+    if args.email:
+        assert isfile(logfilename) and access(logfilename, R_OK), \
+        "File {} doesn't exist or isn't readable".format(logfilename)
+        email_client_name = "mutt"
+        path_to_cmd = shutil.which(email_client_name)    
+        if path_to_cmd is None:
+            mylogs.error(f"mutt email client is not installed.")
+        else:
+            mylogs.info("Sending log %s via email to %s" %(logfilename, suma_data["notify_email"]))
+            subject = "SUSE Manager - Live Patching patch/updates deployment logs."
+            cmd1 = ['cat', logfilename]
+            proc1 = subprocess.run(cmd1, stdout=subprocess.PIPE)
+            cmd2 = [email_client_name, '-s', subject, suma_data["notify_email"]]
+            proc2 = subprocess.run(cmd2, input=proc1.stdout)
+    else:
+        mylogs.info("Not sending email.")
+    return
+
 def printdict(dict_object):
-    print("Item---------------------------------------------")
+    mylogs.info("Item---------------------------------------------")
     for a, b in dict_object.items():
         if isinstance(b, dict):
             for k, v in b.items():
@@ -60,21 +115,21 @@ def printdict(dict_object):
         else:
             print("{:<20}".format(a), "{:<20}".format(b))
         
-    print("----------------------------------------------------")
+    mylogs.info("----------------------------------------------------")
 
 def getpkg_servers_lists(mylist):
     patch_synopsis = "important: Security update for the Linux Kernel (Live Patch"
 
     temp_patch_list = []
     temp_server_list = []
-    for i in mylist:
+    for i, j in mylist.items():
         try:
             temp_list = session.system.getRelevantErrata(key, i)
         except:
-            print("failed to obtain patch list from %s" %(i))
+            mylogs.error("failed to obtain patch list from %s" %(j))
         for s in temp_list:
             if s['advisory_synopsis'].startswith(patch_synopsis):
-                print(s['advisory_synopsis'], " : ", s['id'], " for systemid ", i)
+                mylogs.info("Patch %s will be deployed to %s"%(s['advisory_synopsis'], j))
                 temp_patch_list.append(s['id'])
                 temp_server_list.append(i)
     final_pkg_list = list(set(temp_patch_list))
@@ -88,32 +143,33 @@ def isNotBlank(myString):
     #myString is None OR myString is empty or blank
     return False
 
-def schedule_klp_upgrade(suma_data, groupname):
+def schedule_klp_upgrade(groupname):
     
     nowlater = datetime.datetime.now()
     earliest_occurrence = DateTime(nowlater)
     try:
         result_systemlist = session.systemgroup.listSystemsMinimal(key, groupname)
     except Exception as e:
-        print("get systems list from group failed. %s" %(e))
+        mylogs.error("get systems list from group failed. %s" %(e))
+        result2email()
         exit(1)
-    print("Scheduling SUSE Live Patching patches.")
+    mylogs.info("Scheduling SUSE Live Patching patches.")
 
-    server_id_list = []
+    server_list = {}
     patch_list = []
     for a in result_systemlist:
-        server_id_list.append(a['id'])
+        server_list[a['id']] = a["name"]
        
-    patch_list, server_id_list = getpkg_servers_lists(server_id_list)
+    patch_list, server_id_list = getpkg_servers_lists(server_list)
     # print(patch_list, server_id_list)
     if len(patch_list) and len(server_id_list) > 0:
         try:
             result_job = session.system.scheduleApplyErrata(key, server_id_list, patch_list, earliest_occurrence, True, True)
-            print("Jobs created %s" %(result_job))
+            mylogs.info("Jobs created %s" %(result_job))
         except Exception as e:
-            print("scheduling job failed %s." %(e))
+            mylogs.error("scheduling job failed %s." %(e))
     else:
-        print("Nothing to install. Either already installed or channels not available to the systems.")
+        mylogs.info("Nothing to install. Either already installed or channels not available to the systems.")
     return "finished."
 
 
@@ -127,10 +183,12 @@ else:
 
 if isNotBlank(args.group):
     
-    result = schedule_klp_upgrade(suma_data, args.group)
-    print(result)
+    result = schedule_klp_upgrade(args.group)
+    mylogs.info(result)
 else:
-    print("group name is empty.")
+    mylogs.info("group name is empty.")
+    result2email()
     exit(1)
     
 suma_logout(session, key)
+result2email()
